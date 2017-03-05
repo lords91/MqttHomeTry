58ef7c378469af7dcd417b0b5ebcfcdbbb14824a
lords91 <claudio.cavallaro91@gmail.com>
2017-03-05 10:47:29 +0100

    ...

--- app/src/main/java/lords91/raspy/domotic/MainActivity.java
+++ app/src/main/java/lords91/raspy/domotic/MainActivity.java
@@ -3,9 +3,18 @@ package lords91.raspy.domotic;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -32,5 +41,60 @@ public class MainActivity extends AppCompatActivity {
                 off.execute();
             }
         });
+
+
+
+        //Connection to MQTT Server
+
+        final MqttAndroidClient mqttAndroidClient = new MqttAndroidClient(this.getApplicationContext(), "tcp://192.168.1.9:1883", "androidSampleClient");
+        mqttAndroidClient.setCallback(new MqttCallback() {
+
+            TextView tv = (TextView) findViewById(R.id.textViewTemp);
+
+            @Override
+            public void connectionLost(Throwable cause) {
+                System.out.println("Connection was lost!");
+
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                System.out.println("Message Arrived!: " + topic + ": " + new String(message.getPayload()));
+                String temp = new String(message.getPayload());
+                tv.setText(temp + "° C");
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+                System.out.println("Delivery Complete!");
+            }
+        });
+
+        try {
+            mqttAndroidClient.connect(null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    System.out.println("Connection Success!");
+                    try {
+                        System.out.println("Subscribing to /test");
+                        mqttAndroidClient.subscribe("/test", 0);
+                        System.out.println("Subscribed to /test");
+                        //System.out.println("Publishing message..");
+                        //mqttAndroidClient.publish("/test", new MqttMessage("Hello world!".getBytes()));
+                    } catch (MqttException ex) {
+
+                    }
+
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    System.out.println("Connection Failure!");
+                }
+            });
+        } catch (MqttException ex) {
+
+        }
+
     }
 }
